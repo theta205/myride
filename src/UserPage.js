230142195fa282271859cb6fb15ff7a7aa60b312
Isaiah Rohrer <isaiahrohrer@wc-dhcp0d251.student-secure.wireless.fsu.edu>
2024-09-24 15:56:25 -0400

    built start of profile temp

--- src/UserPage.js
+++ src/UserPage.js
@@ -3,6 +3,16 @@ import { useParams } from 'react-router-dom';
 import { useUser } from '@clerk/clerk-react';
 import './page.css'
 import miata from './images/miata.png'
+
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 const UserPage = () => {
   const { username } = useParams(); // Get the username from the URL
   const { user } = useUser();
@@ -14,40 +24,66 @@ const UserPage = () => {
   return (
     // <div>
     //   <h1>User Page</h1>
-    //   <p>Your email: {user.primaryEmailAddress.emailAddress}</p>
+    //     <div class="header">
+  //   <img src="gear.png" alt="Gear Icon" />
+  //   <img src="menu.png" alt="Menu Icon" />
+  // </div> <p>Your email: {user.primaryEmailAddress.emailAddress}</p>
     //   <p>Viewing page for username: {username}</p> {/* Display the username */}
     //   {/* You can add more user-specific information here */}
     // </div>
-    <div class="container">
-  <div class="car-info">
-    <div class="stats-lander">
-    <div class="header">
-      <img src="gear.png" alt="Gear Icon" />
-      <img src="menu.png" alt="Menu Icon" />
-    </div>
-    <img src={miata} class="car-img" alt="Purple Mazda Miata" />
-    <h1>1999 Mazda Miata</h1>
-    <div class="specs">
-      <h2>140 hp</h2>
-      <h2>119 ft/lbs</h2>
-      <h2>RWD</h2>
-    </div>
-    </div>
-    <div class="hashtags">
-      <p>#Stance #JDM #LowBoys #NA #DropTop</p>
-    </div>
-    <div class="buttons">
-      <button class="button">Performance</button>
-      <button class="button">Cosmetics</button>
-    </div>
-    <div class="performance-part">
-      <img src="air_filter.png" alt="Air Filter Icon" />
-      <h3>HPS Performance Cold Air Intake</h3>
-    </div>
-    <img src="anime_girl.png" class="car-img" alt="Anime Girl on Branch" />
-  </div>
+    
+<Container className="d-flex justify-content-center vh-100">
+  <Row>
+    <Col>
+      <div className='bg '> 
+        <div className="stats-lander " style = {{width:'600px'}}>
+          <Image 
+            src={miata}
+            className="img-fluid"
+            style={{
+              width: "auto", 
+              height: "auto",
+              borderBottomLeftRadius: "66px", 
+              borderBottomRightRadius: "66px"
+            }} 
+          />
+          <div style={{ textAlign: 'center' }}>
+            <h1>1999 Mazda Miata</h1>
+          </div>
+          <div className="stats-oval text-center">
+            <Row className="justify-content-center ">
+              <Col>140 hp</Col>
+              <Col>119 ft/lbs</Col>
+              <Col>RWD</Col>
+            </Row>
+          </div>
+        </div>
+        <div className="toggle-container" style={{ paddingTop: '20px' }}>
+  <label className="toggle-button">
+    <input type="checkbox" />
+    <span className="slider">
+      <span className="toggle-label off">Cosmetics</span>
+      <span className="toggle-label on">Performance</span>
+    </span>
+  </label>
 </div>
 
+        </div>
+    </Col>
+  </Row>        
+        {/* <div class="hashtags">
+          <p>#Stance #JDM #LowBoys #NA #DropTop</p>
+        </div>
+        <div class="buttons">
+          <button class="button">Performance</button>
+          <button class="button">Cosmetics</button>
+        </div>
+        <div class="performance-part">
+          <img src="air_filter.png" alt="Air Filter Icon" />
+          <h3>HPS Performance Cold Air Intake</h3>
+        </div>
+        <img src="anime_girl.png" class="car-img" alt=" on Branch" /> */}
+    </Container>
   
 
   );
---
+++ src/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Image from 'react-bootstrap/Image';
+
+const Page = () => {
+    return 
+}
+export  default Page;
